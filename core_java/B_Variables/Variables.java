89eafdc690a8550cb2c91c9d8de1e4df337e0170
Huzaifa-Ansari222 <huzaifaansari656@gmail.com>
2025-04-04 20:19:37 +0530

    added files

---
+++ core_java/B_Variables/Variables.java
@@ -0,0 +1,26 @@
+package core_java.B_Variables;
+
+public class Variables {
+
+    // 1. Static variable (class level/ global var)
+    static int staticVar = 10;
+
+    // 2. Non-static variable (instance level)
+    int nonStatic = 20;
+
+    public static void main(String[] args) {
+
+        // 3. Local variable (defined inside a method & acess with in {} block)
+        int localVar = 30;
+
+        // 4. Reference variable (refers to an object)
+        Variables obj = new Variables(); // reference variable
+
+        System.out.println("Static Variable: " + staticVar);      // Accessing static directly
+        System.out.println("Non-Static Variable: " + obj.nonStatic); // Accessing non-static via object
+        System.out.println("Local Variable: " + localVar);        // Accessing local variable
+        System.out.println("Reference Variable points to: " + obj); // Printing reference variable
+
+    }
+
+}
---
+++ core_java/C_Memory_Management/StackHeap.java
@@ -0,0 +1,35 @@
+package core_java.C_Memory_Management;
+
+public class StackHeap {
+    public static void main(String[] args) {
+        // Object created in HEAP
+        String str1 = new String("Hello");
+
+        // Reference vars in STACK
+        String str2 = str1;  // Both point to same object
+        System.out.println(str1);//hello
+        System.out.println(str2);//hello
+
+        //print address both have same address
+        System.out.println("str1 hash: " + System.identityHashCode(str1));
+        System.out.println("str2 hash: " + System.identityHashCode(str2));
+
+        str1 = null;  // Object now eligible for GC
+        System.out.println("str1 to null: " + System.identityHashCode(str1));//0
+        //System.gc(); // Just a request, JVM may ignore it.
+
+    }
+}
+
+/*
+🗑️ What Garbage Collector (GC) does:
+-It marks that heap object as "eligible for garbage collection".
+-But it doesn’t delete it instantly!
+-The GC runs automatically at some later point (when JVM decides it's needed — like low memory or idle time).
+-Then it cleans up such unreferenced objects to free memory.
+
+❗ Important Points:
+-str = null does not immediately delete the object.
+-GC only cleans up unreachable objects, not ones still referenced.
+-You cannot manually force GC, but you can suggest it:
+ */
