e15fc89847c7e9b5c3e6befd3650856e74b48077
Huzaifa-Ansari222 <huzaifaansari656@gmail.com>
2025-04-18 19:01:54 +0530

    added files

---
+++ core_java/B_Variables/Var_Type.java
@@ -0,0 +1,25 @@
+package core_java.B_Variables;
+
+public class Var_Type {
+//    var z = 10;//error
+//    static var z1 = 30;// error
+
+
+    public static void main(String[] args) {
+        //var java version n10
+        //var hold any data value
+        //var isnt key word
+        //can be only used in local variable not in nonstatic or static
+        var x = 10;
+        var x1 = 'H';
+        var x2 = "huzaifa34@";
+        var x3 = 20.33;
+        var var = 333; // var type isnot reseverd
+
+        System.out.println(x);
+        System.out.println(x1);
+        System.out.println(x2);
+        System.out.println(x3);
+
+    }
+}
